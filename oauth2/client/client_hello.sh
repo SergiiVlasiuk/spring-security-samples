e902c47d432fb7d2a016f73e805d8debe6e9e297
sergii <sergii_vlasiuk@ukr.net>
2020-03-23 22:51:20 +0200

    refactoring - simplify.

--- oauth2/client/client_hello.sh
+++ oauth2/client/client_hello.sh
@@ -4,8 +4,8 @@
 curl localhost:8090/hello/me
 echo
 
-#curl localhost:8090/hello/authorized
-#echo
+curl localhost:8090/hello/authorized
+echo
 
 TOKEN=`curl -s -u guest_app:secret -X POST localhost:8090/oauth/token\?grant_type=client_credentials | egrep -o '[a-f0-9-]{20,}'`
 echo "Got token for curl client as : $TOKEN"
--- oauth2/oauth2-server-inmemory-example/src/main/java/org/vl/oauth2/inmemory/example/web/controller/HelloController.java
+++ oauth2/oauth2-server-inmemory-example/src/main/java/org/vl/oauth2/inmemory/example/web/controller/HelloController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("hello")
 public class HelloController {
@@ -17,8 +19,8 @@ public class HelloController {
     }
 
     @GetMapping("me")
-//    public Principal me(@CurrentSecurityContext(expression = "authentication.principal") Principal security) {
-    public Object me(@CurrentSecurityContext Object security) {
+    public Principal me(Principal security) {
+//    public Object me(@CurrentSecurityContext Object security) {
         return security;
     }
 }
